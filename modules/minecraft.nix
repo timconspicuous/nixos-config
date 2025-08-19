4e1135b2f10bad8324f535b1757e88771c32e9a2
timconspicuous <git@timtinkers.online>
2025-08-19 19:50:53 +0200

    Updated Minecraft whitelist

--- modules/minecraft.nix
+++ modules/minecraft.nix
@@ -50,6 +50,7 @@
       whitelist = {
         timconspicuous = "4db27365-dbe0-4fd7-a380-10afba6b832c";
         MoinIzzy = "a7d0a634-5cbc-4a9e-a811-7a1bf6d80354";
+        SirCharlio = "a2ae2a03-f0ca-4cf6-93d2-d95e1940072e";
       };
     };
   };
