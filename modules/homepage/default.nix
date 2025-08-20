6a42a5027ede75a85da59696a61baba94e5528d2
timconspicuous <git@timtinkers.online>
2025-08-21 01:38:20 +0200

    Refactor to make Nginx modular

--- modules/homepage/default.nix
+++ modules/homepage/default.nix
@@ -1,15 +1,44 @@
-{ lib, ... }:
+{ config, lib, ... }:
 
 with lib;
 
+let
+  cfg = config.services.homelab.homepage;
+in
 {
-  # Configure the homepage-dashboard service
-  config = {
+  options.services.homelab.homepage = {
+    enable = mkEnableOption "Homepage Dashboard";
+
+    listenPort = mkOption {
+      type = types.port;
+      default = 8082;
+      description = "Port for homepage dashboard";
+    };
+
+    enableReverseProxy = mkOption {
+      type = types.bool;
+      default = true;
+      description = "Enable reverse proxy for this service";
+    };
+  };
+
+  config = mkIf cfg.enable {
+    # Configure the actual homepage-dashboard service
     services.homepage-dashboard = {
-      enable = mkEnableOption "Start Homepage Dashbpard";
+      enable = true;
       bookmarks = import ./bookmarks.nix;
       settings = import ./settings.nix;
       services = import ./services.nix;
+      listenPort = cfg.listenPort;
+    };
+
+    # Register with nginx only if reverse proxy is enabled
+    services.myNginx.reverseProxies = mkIf cfg.enableReverseProxy {
+      homepage = {
+        subdomain = "home";
+        target = "http://127.0.0.1:${toString cfg.listenPort}";
+        websockets = true;
+      };
     };
   };
 }
--- modules/nginx.nix
+++ modules/nginx.nix
@@ -1,53 +1,153 @@
-{ ... }:
+{ config, lib, ... }:
 
+with lib;
+
+let
+  cfg = config.services.myNginx;
+in
 {
-  services.nginx = {
-    enable = true;
+  options.services.myNginx = {
+    enable = mkEnableOption "custom nginx configuration";
 
-    virtualHosts."auth.timtinkers.online" = {
-      enableACME = true;
-      forceSSL = true;
-      locations."/" = {
-        proxyPass = "http://127.0.0.1:9091";
-        proxyWebsockets = true; # needed if you need to use WebSocket
-        extraConfig =
-          # required when the target is also TLS server with multiple hosts
-          "proxy_ssl_server_name on;"
-          +
-            # required when the server wants to use HTTP Authentication
-            "proxy_pass_header Authorization;";
-      };
+    domain = mkOption {
+      type = types.str;
+      default = "timtinkers.online";
+      description = "Base domain name";
     };
 
-    # TCP/UDP traffic
-    appendConfig = ''
-      stream {
-        # Map subdomain to backend server
-        map $ssl_preread_server_name $backend_pool {
-          minecraft.timtinkers.online 127.0.0.1:25566;
-          modded.timtinkers.online 127.0.0.1:25567;
-          # Default fallback
-          default 127.0.0.1:25566;
+    # HTTP reverse proxy services
+    reverseProxies = mkOption {
+      type = types.attrsOf (
+        types.submodule {
+          options = {
+            subdomain = mkOption {
+              type = types.str;
+              description = "Subdomain name";
+            };
+            target = mkOption {
+              type = types.str;
+              description = "Target URL to proxy to";
+            };
+            websockets = mkOption {
+              type = types.bool;
+              default = false;
+              description = "Enable WebSocket support";
+            };
+            extraConfig = mkOption {
+              type = types.str;
+              default = "";
+              description = "Extra nginx location configuration";
+            };
+          };
         }
+      );
+      default = { };
+      description = "HTTP reverse proxy configurations";
+    };
 
-        server {
-          listen 25565;
-          proxy_pass $backend_pool;
-          proxy_timeout 1s;
-          proxy_responses 1;
-
-          # Enable SSL preread to get server name
-          ssl_preread on;
-
-          error_log /var/log/nginx/gameserver_error.log;
+    # TCP/UDP stream proxies
+    streamProxies = mkOption {
+      type = types.attrsOf (
+        types.submodule {
+          options = {
+            subdomain = mkOption {
+              type = types.str;
+              description = "Subdomain name for SSL preread";
+            };
+            target = mkOption {
+              type = types.str;
+              description = "Target host:port to proxy to";
+            };
+            port = mkOption {
+              type = types.port;
+              description = "Port to listen on";
+            };
+          };
         }
-      }
-    '';
+      );
+      default = { };
+      description = "TCP/UDP stream proxy configurations";
+    };
+
+    # Catch-all 404 page
+    defaultSite = mkOption {
+      type = types.bool;
+      default = true;
+      description = "Enable catch-all default site with 404";
+    };
   };
 
-  networking.firewall.allowedTCPPorts = [
-    80
-    443
-    25565
-  ];
+  config = mkIf cfg.enable {
+    services.nginx = {
+      enable = true;
+
+      # Generate virtualHosts from reverseProxies
+      virtualHosts =
+        # Individual reverse proxy hosts
+        (mapAttrs (name: proxy: {
+          serverName = "${proxy.subdomain}.${cfg.domain}";
+          enableACME = true;
+          forceSSL = true;
+          locations."/" = {
+            proxyPass = proxy.target;
+            proxyWebsockets = proxy.websockets;
+            extraConfig = "proxy_ssl_server_name on;" + "proxy_pass_header Authorization;" + proxy.extraConfig;
+          };
+        }) cfg.reverseProxies)
+
+        //
+
+          # Default catch-all site
+          (optionalAttrs cfg.defaultSite {
+            "_" = {
+              serverName = "_.${cfg.domain}";
+              default = true;
+              enableACME = true;
+              forceSSL = true;
+              locations."/" = {
+                return = "404";
+                extraConfig = ''
+                  add_header Content-Type text/plain;
+                  return 404 "Service not found";
+                '';
+              };
+            };
+          });
+
+      # Generate stream configuration if we have stream proxies
+      appendConfig = mkIf (cfg.streamProxies != { }) ''
+        stream {
+          # Map subdomain to backend server
+          map $ssl_preread_server_name $backend_pool {
+            ${concatStringsSep "\n    " (
+              mapAttrsToList (name: proxy: "${proxy.subdomain}.${cfg.domain} ${proxy.target};") cfg.streamProxies
+            )}
+            # Default fallback to first proxy or deny
+            default ${
+              if cfg.streamProxies != { } then (head (attrValues cfg.streamProxies)).target else "127.0.0.1:1" # Will fail, no default
+            };
+          }
+
+          ${concatStringsSep "\n  " (
+            mapAttrsToList (name: proxy: ''
+              server {
+                listen ${toString proxy.port};
+                proxy_pass $backend_pool;
+                proxy_timeout 1s;
+                proxy_responses 1;
+                ssl_preread on;
+                error_log /var/log/nginx/${name}_error.log;
+              }
+            '') (groupBy (proxy: toString proxy.port) (attrValues cfg.streamProxies))
+          )}
+        }
+      '';
+    };
+
+    networking.firewall.allowedTCPPorts = [
+      80 # HTTP
+      443 # HTTPS
+      25565 # Minecraft (external port for all servers)
+    ];
+  };
 }
